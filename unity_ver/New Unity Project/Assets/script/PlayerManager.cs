c36d35d3f3540df424e6cdee9f59d676c3d010a7
korleinster <leinster92@me.com>
2017-08-31 16:31:44 +0900

    [신창섭]유니티 프로젝트 생성
    1.뀨!

---
+++ unity_ver/New Unity Project/Assets/script/PlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+	void Update ()
+    {
+        // 왼쪽 입력 시에
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+        {
+            Vector2 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+
+            anim.SetInteger("state", 1);
+        }
+        if (Input.GetKeyUp (KeyCode.LeftArrow))
+        {
+            Vector2 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+
+            anim.SetInteger("state", 0);
+        }
+
+        // 오른쪽 입력 시에
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector2 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+
+            anim.SetInteger("state", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Vector2 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+
+            anim.SetInteger("state", 0);
+        }
+
+    }
+}
