fa5bb934fb2bb94f32edfc598452cef9ee21d67d
korleinster <leinster92@me.com>
2017-09-01 02:51:31 +0900

    [신창섭]유니티 백업
    1.유감

---
+++ unity_ver/New Unity Project/Assets/script/LoadScreen.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour {
+
+    [SerializeField]
+    private float units;
+
+    [SerializeField]
+    private GameObject createPrefab;
+
+    [SerializeField]
+    private Image fill;
+
+    private float fillAmount;
+
+    [SerializeField]
+    private GameObject button;
+
+	void Start ()
+    {
+        StartCoroutine(BuildUnits());
+	}
+	
+	void Update ()
+    {
+        UpdateBar();
+	}
+
+    public IEnumerator BuildUnits()
+    {
+        Vector2 topLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 1));
+        Vector2 bottomRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
+
+        for (int i = 0; i <= units; i++)
+        {
+            Vector2 randomPos = new Vector2(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y));
+
+            fillAmount = i / units;
+
+            Instantiate(createPrefab, randomPos, Quaternion.identity);
+            yield return null;
+        }
+
+        // 로딩이 완료되었을 때!
+
+        button.SetActive(true);
+    }
+
+    private void UpdateBar()
+    {
+        fill.fillAmount = fillAmount;
+    }
+
+    public void StartGame()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- unity_ver/New Unity Project/Assets/script/PlayerManager.cs
+++ unity_ver/New Unity Project/Assets/script/PlayerManager.cs
@@ -6,17 +6,20 @@ public class PlayerManager : MonoBehaviour
 {
     Animator anim;
 
+    public float MoveSpeed;
+
 	void Start ()
     {
-        anim = GetComponent<Animator>();
+        Screen.SetResolution(1280, 720, true); // 플레이 해상도 고정
+        anim = GetComponent<Animator>(); // 스프라이트 재생용 애니메이터 생성
 	}
 
 	void Update ()
     {
         // 왼쪽 입력 시에
-		if (Input.GetKeyDown (KeyCode.LeftArrow))
+        if (Input.GetKeyDown (KeyCode.LeftArrow))
         {
-            Vector2 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale;
 
@@ -24,7 +27,7 @@ public class PlayerManager : MonoBehaviour
         }
         if (Input.GetKeyUp (KeyCode.LeftArrow))
         {
-            Vector2 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale;
 
@@ -34,7 +37,7 @@ public class PlayerManager : MonoBehaviour
         // 오른쪽 입력 시에
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Vector2 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
             scale.x = -Mathf.Abs(scale.x);
             transform.localScale = scale;
 
@@ -42,7 +45,7 @@ public class PlayerManager : MonoBehaviour
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            Vector2 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
             scale.x = -Mathf.Abs(scale.x);
             transform.localScale = scale;
 
